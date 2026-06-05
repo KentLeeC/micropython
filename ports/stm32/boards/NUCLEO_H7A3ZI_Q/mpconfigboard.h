6228205dbd3ceedb28828e6bdee5adb9e832e107
KentLeeC <kent_lee@goglobal.com.tw>
2026-06-05 14:53:37 +0800

    Enable machine.I2S module in mpconfigboard.h
    Signed-off-by: KentLeeC <kent_lee@goglobal.com.tw>

--- ports/stm32/boards/NUCLEO_H7A3ZI_Q/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_H7A3ZI_Q/mpconfigboard.h
@@ -114,6 +114,9 @@
 #define MICROPY_HW_CAN1_RX                  (pin_D0)
 
 // 強制開啟 machine.I2S 模組
+#ifdef MICROPY_PY_MACHINE_I2S
+#undef MICROPY_PY_MACHINE_I2S
+#endif
 #define MICROPY_PY_MACHINE_I2S (1)
 
 void NUCLEO_H7A3ZIQ_board_early_init(void);
